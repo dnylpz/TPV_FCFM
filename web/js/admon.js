25e25d3ac3a848cefa98134db7c1065e3259c617
unknown <jose.espinoza.lopez@MTYMT15065WYG.dir.svc.accenture.com>
2015-09-25 16:26:53 -0500

    changes on views html

--- web/js/admon.js
+++ web/js/admon.js
@@ -2,27 +2,11 @@
  * Created by jose.espinoza.lopez on 9/17/2015.
  */
 $(document).ready(function(){
-
-});
-
-$('#users').on('click',function(event){
-  event.preventDefault();
-  console.log("you clicked the link basterd");
-  console.log
-  $.ajax('templates/admin/users.html',{
-    success: function(response){
-      console.log("i got your file basterd won't give it to you now");
-      $('.content').hide();
-    },
-    error: function(response, errorType, errormsg){
-      console.log('Error: ' + errorType + ' ' + errormsg);
-    },
-    timeout: 3000,
-    beforeSend: function(){
-      $('.content').hide();
-    },
-    complete: function(){
-
-    }
+  console.log("hello world");
+  $('#users').on('click', function(){
+    $.get('templates/admin/users.html',function(response){
+      $('.content').children().slideUp();
+      $('.content').html(response);
+    });
   });
 });
