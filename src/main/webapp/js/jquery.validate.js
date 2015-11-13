ac1df441c568bc81c4aeffe75ea060395e793beb
Espinoza Lopez, Jose <jose.espinoza.lopez@accenture.com>
2015-11-12 21:05:26 -0600

    termina todas las funciones de agregar al ticket, falta completar cobro, reportes, y refactorear usuarios y productos para agregar detalle requerido, falta añadir clientes, impuestos, y promociones y editar video y horarios, sucursales y algunos datos de las empresas

--- src/main/webapp/js/jquery.validate.js
+++ src/main/webapp/js/jquery.validate.js
@@ -8,7 +8,7 @@
  */
 (function( factory ) {
     if ( typeof define === "function" && define.amd ) {
-        define( ["jquery"], factory );
+        define( ["jQuery"], factory );
     } else {
         factory( jQuery );
     }
